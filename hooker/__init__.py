3cb8bc2652048c6a2ca9a5072f1bb5c26b8d6677
Dimitris Zervas <dzervas@dzervas.gr>
2018-04-25 23:22:07 +0300

    Version bump and setup.py changes

--- hooker/__init__.py
+++ hooker/__init__.py
@@ -1,7 +1,8 @@
 from .api import *
 
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __name__ = 'hooker'
 __desc__ = 'An arcane-powered python hooking library for sane humans'
 __email__ = 'satori_ng@email.com'
+__github__ = 'https://github.com/satori-ng/hooker'
--- setup.py
+++ setup.py
@@ -2,13 +2,36 @@ from setuptools import setup
 
 import hooker
 
+try:
+    from pypandoc import convert
+
+    def read_md(name):
+        # Hack to pass the "rst_lint.py" - PyPI
+        return convert(name, "rst").replace("~", "^")
+
+except ImportError:
+    print("Warning: pypandoc module not found, could not convert MD to RST")
+
+    def read_md(name):
+        return open(name, "r").read()
+
+try:
+    LONG_DESCRIPTION = read_md("README.md")
+except IOError:
+    LONG_DESCRIPTION = hooker.__github__ + "/blob/master/README.md"
+
+
 setup(
     name=hooker.__name__,
     description=hooker.__desc__,
+    long_description=LONG_DESCRIPTION,
     version=hooker.__version__,
 
     author="Satori-NG org",
     author_email=hooker.__email__,
 
+    url=hooker.__github__,
+
     packages=["hooker"],
+    keywords=["hook", "event", "plugin", "extension", "module"]
 )
