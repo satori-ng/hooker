821224861c7d05229a2c3a81cf2c0854d71bc4c9
Dimitris Zervas <dzervas@dzervas.gr>
2018-10-28 07:23:25 +0200

    Theme change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,15 +18,15 @@
 
 
 # -- Project information -----------------------------------------------------
+import hooker
 
-project = 'Hooker'
-copyright = '2018, Dimitris Zervas'
-author = 'Dimitris Zervas'
+project = hooker.__name__
+author = hooker.__author__
 
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = hooker.__version__
 
 
 # -- General configuration ---------------------------------------------------
@@ -78,7 +78,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
