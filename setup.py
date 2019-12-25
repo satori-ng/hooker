a5d88f1053a0da5226656c1c189dbc381af558c3
Dimitris Zervas <dzervas@dzervas.gr>
2019-12-25 16:53:01 +0200

    Fix deployments

--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
+from pypandoc import convert
 from setuptools import setup
 
 import hooker
 
-try:
-    from pypandoc import convert
 
-    def read_md(name):
-        # Hack to pass the "rst_lint.py" - PyPI
-        return convert(name, "rst").replace("~", "^")
+def read_md(name):
+    # Hack to pass the "rst_lint.py" - PyPI
+    return convert(name, "rst").replace("~", "^")
 
-except ImportError:
-    print("Warning: pypandoc module not found, could not convert MD to RST")
-
-    def read_md(name):
-        return open(name, "r").read()
 
 try:
     LONG_DESCRIPTION = read_md("README.md")
 except IOError:
     LONG_DESCRIPTION = hooker.__github__ + "/blob/master/README.md"
+
+if not hasattr(hooker, "__version__"):
+    hooker.__version__ = "develop"
 
 
 setup(
