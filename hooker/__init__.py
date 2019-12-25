26c9d6a89cf2969030845ac87b40ee78512a5192
Dimitris Zervas <dzervas@dzervas.gr>
2019-12-25 16:45:42 +0200

    Automated the whole versioning madness

--- hooker/__init__.py
+++ hooker/__init__.py
@@ -2,7 +2,6 @@ from .api import *
 from .hook_list import HookException
 
 
-__version__ = '0.4.2'
 __name__ = 'hooker'
 __desc__ = 'An arcane-powered python hooking library for sane humans'
 __email__ = 'satori_ng@email.com'
